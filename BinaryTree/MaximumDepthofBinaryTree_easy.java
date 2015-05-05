207aa37234d78053ccc4d696acd19c62872e3baf
Peng <eamonpeng91@gmail.com>
2015-05-05 15:05:16 -0400

    Create MaximumDepthofBinaryTree_easy.java

---
+++ BinaryTree/MaximumDepthofBinaryTree_easy.java
@@ -0,0 +1,41 @@
+/*
+ * Maximum Depth of Binary Tree
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+package BinaryTree;
+
+public class MaxDepthofBTSol {
+	public static int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+		return Math.max(left, right) + 1;
+	}
+	
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		public TreeNode (int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String arg[]) {
+		TreeNode t1 = new TreeNode(1);
+		TreeNode t2 = new TreeNode(2);
+		TreeNode t3 = new TreeNode(3);
+		TreeNode t4 = new TreeNode(4);
+		t1.left = t2;
+		t1.right = t3;
+		t2.left = t4;
+		int depth = maxDepth(t1);
+		System.out.print(depth);
+	}
+}
+/*
+output: 3
+*/
