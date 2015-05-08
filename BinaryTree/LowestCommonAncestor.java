74fb21ee44d34b664f1100f0eb022488f4d7fec9
Peng <eamonpeng91@gmail.com>
2015-05-08 10:40:55 -0400

    Create LowestCommonAncestor.java

---
+++ BinaryTree/LowestCommonAncestor.java
@@ -0,0 +1,14 @@
+/*
+Lowest Common Ancestor
+Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+Example
+        4
+    /     \
+  3         7
+          /     \
+        5         6
+For 3 and 5, the LCA is 4.
+For 5 and 6, the LCA is 7.
+For 6 and 7, the LCA is 7.
+*/
