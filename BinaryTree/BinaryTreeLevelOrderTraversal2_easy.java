4bb6a853e7883b12b0f4af66adfbbb786e9998e8
Peng <eamonpeng91@gmail.com>
2015-05-08 18:00:40 -0400

    Update BinaryTreeLevelOrderTraversal2_easy.java

--- BinaryTree/BinaryTreeLevelOrderTraversal2_easy.java
+++ BinaryTree/BinaryTreeLevelOrderTraversal2_easy.java
@@ -15,3 +15,59 @@ return its bottom-up level order traversal as:
   [3]
 ]
 */
+public class BTLevelOrderT2Sol {
+	public static List<List<Integer>> levelOrder(TreeNode root) {
+		List rst = new ArrayList();
+		
+		if (root == null) {
+			return rst;
+		}
+		
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			List<Integer> level = new ArrayList<Integer>();
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode head = queue.poll();
+				level.add(head.val);
+				if (head.left != null) {
+					queue.offer(head.left);
+				}
+				if (head.right != null) {
+					queue.offer(head.right);
+				}
+			}
+			rst.add(level);
+		}
+		Collections.reverse(rst);
+		return rst;
+	}
+	
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String arg[]) {
+		TreeNode t1 = new TreeNode(3);
+		TreeNode t2 = new TreeNode(9);
+		TreeNode t3 = new TreeNode(20);
+		TreeNode t4 = new TreeNode(15);
+		TreeNode t5 = new TreeNode(7);
+		t1.left = t2;
+		t1.right = t3;
+		t3.left = t4;
+		t3.right = t5;
+		List rst = new ArrayList();
+		rst = levelOrder(t1);
+		System.out.print(rst);
+	}
+}
+/*
+ * output: [[15, 7], [9, 20], [3]]
+ */
