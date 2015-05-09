b16e8ee864336a53f3e9f1f752b3257a02c3a44c
Peng <eamonpeng91@gmail.com>
2015-05-09 01:20:32 -0400

    Update BinaryTreeBFSTemplate.java

--- BinaryTree/BinaryTreeBFSTemplate.java
+++ BinaryTree/BinaryTreeBFSTemplate.java
@@ -2,6 +2,30 @@
 Binary Tree BFS Template
 */
 public class Solution {
-
-
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List rst = new ArrayList();
+        
+        if (root == null) {
+            return rst;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                } 
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            rst.add(level);
+        }
+        return rst;
+    }
 }
