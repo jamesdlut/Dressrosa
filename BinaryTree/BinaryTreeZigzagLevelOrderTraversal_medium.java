b689898f4e795b510faea57458123bdaa2066c89
Peng <eamonpeng91@gmail.com>
2015-05-09 01:13:17 -0400

    Update BinaryTreeZigzagLevelOrderTraversal_medium.java

--- BinaryTree/BinaryTreeZigzagLevelOrderTraversal_medium.java
+++ BinaryTree/BinaryTreeZigzagLevelOrderTraversal_medium.java
@@ -15,3 +15,62 @@ return its zigzag level order traversal as:
   [15,7]
 ]
 */
+public class BTZLevelOrderTSol {
+	public static List<List<Integer>> zigzaglevelOrder(TreeNode root) {
+		List rst = new ArrayList();
+		if (root == null) {
+			return rst;
+		}
+		
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		int j = 1;
+		while (!queue.isEmpty()) {
+			List<Integer> level = new ArrayList<Integer>();
+			int size = queue.size();
+			int i;
+			for (i = 0; i < size; i++) {
+				TreeNode head =queue.poll();
+				level.add(head.val);
+				if (head.left != null) {
+					queue.offer(head.left);
+				}
+				if (head.right != null) {
+					queue.offer(head.right);
+				}
+			}
+			if ((j - 1) % 2 == 0) {
+				j++;
+			} else {
+				j++;
+				Collections.reverse(level);
+			}
+			rst.add(level);
+		}
+		return rst;
+	}
+	
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String arg[]) {
+		TreeNode t1 = new TreeNode(3);
+		TreeNode t2 = new TreeNode(9);
+		TreeNode t3 = new TreeNode(20);
+		TreeNode t4 = new TreeNode(15);
+		TreeNode t5 = new TreeNode(7);
+		t1.left = t2;
+		t1.right = t3;
+		t3.left = t4;
+		t3.right = t5;
+		List rst = new ArrayList();
+		rst = zigzaglevelOrder(t1);
+		System.out.print(rst);
+	}
+}
