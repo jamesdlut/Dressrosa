6b444dde31073d959e483e1596d22a80b04b71c7
Peng <eamonpeng91@gmail.com>
2015-05-11 09:08:17 -0400

    Update BinaryTreeInorderTraversal_medium.java

--- BinaryTree/BinaryTreeInorderTraversal_medium.java
+++ BinaryTree/BinaryTreeInorderTraversal_medium.java
@@ -11,3 +11,32 @@ Given binary tree {1,#,2,3},
 return [1,3,2].
 Note: Recursive solution is trivial, could you do it iteratively?
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+   // recursion 
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> rst = new ArrayList<Integer>();
+        rec(root, rst);
+        return rst;
+    }
+    
+    public void rec(TreeNode root, List<Integer> rst) {
+        if (root == null) {
+            return;
+        }
+        
+        rec(root.left, rst);
+        rst.add(root.val);
+        rec(root.right, rst);
+    }
+}  
+   // iteration
+   
