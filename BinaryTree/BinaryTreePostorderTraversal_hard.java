af662ef94417f4a3ba23efba047d63bb5338e01c
Peng <eamonpeng91@gmail.com>
2015-05-11 09:21:05 -0400

    Update BinaryTreePostorderTraversal_hard.java

--- BinaryTree/BinaryTreePostorderTraversal_hard.java
+++ BinaryTree/BinaryTreePostorderTraversal_hard.java
@@ -10,3 +10,34 @@ Given binary tree {1,#,2,3},
    3
 return [3,2,1].
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // recursion
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> rst = new ArrayList<Integer>();
+        if (root == null) {
+            return rst;
+        }
+        
+        dfs(root, rst);
+        return rst;
+    }
+    
+    public void dfs(TreeNode root, List<Integer> rst) {
+        if (root == null) {
+            return;
+        }
+        
+        dfs(root.left, rst);
+        dfs(root.right, rst);
+        rst.add(root.val);
+    }
+}
