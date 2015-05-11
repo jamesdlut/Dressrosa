b8c95cd99a5afcf5d95ef1158ac0f2bcf692dbcc
Peng <eamonpeng91@gmail.com>
2015-05-11 09:32:17 -0400

    Update BinaryTreePostorderTraversal_hard.java

--- BinaryTree/BinaryTreePostorderTraversal_hard.java
+++ BinaryTree/BinaryTreePostorderTraversal_hard.java
@@ -10,6 +10,10 @@ Given binary tree {1,#,2,3},
    3
 return [3,2,1].
 */
+/*
+从左到右的后序与从右到左的前序的逆序是一样的, 用另外一个栈进行翻转即可
+123 -> 132 -> 231
+*/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -39,5 +43,31 @@ public class Solution {
         dfs(root.left, rst);
         dfs(root.right, rst);
         rst.add(root.val);
+    }
+    // iteration
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> rst = new ArrayList<Integer>();
+        if (root == null) {
+            return rst;
+        }
+        
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        Stack<Integer> out = new Stack<Integer>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            TreeNode cur = s.pop();
+            out.push(cur.val);
+            if (cur.left != null) {
+                s.push(cur.left);
+            }
+            if (cur.right != null) {
+                s.push(cur.right);
+            }
+        }
+        
+        while (!out.isEmpty()) {
+            rst.add(out.pop());
+        }
+        return rst;
     }
 }
