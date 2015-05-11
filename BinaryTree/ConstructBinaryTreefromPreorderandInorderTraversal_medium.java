5dd8595a74962da60dbefb21407af3ddcbb8600a
Peng <eamonpeng91@gmail.com>
2015-05-11 13:27:20 -0400

    Update ConstructBinaryTreefromPreorderandInorderTraversal_medium.java

--- BinaryTree/ConstructBinaryTreefromPreorderandInorderTraversal_medium.java
+++ BinaryTree/ConstructBinaryTreefromPreorderandInorderTraversal_medium.java
@@ -9,3 +9,55 @@ You may assume that duplicates do not exist in the tree.
 2. Try to find the position of the root in the inorder. Then we can get the number of nodes in the left tree.
 3. 递归调用，构造左子树和右子树。
 */
+public class ConstructBT2Sol {
+	public static TreeNode buildTree(int[] preorder, int[] inorder) {
+		if (preorder == null || inorder == null || preorder.length == 0 || inorder.length == 0) {
+			return null;
+		}
+		
+		return dfs(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+	}
+	
+	public static TreeNode dfs(int[] preorder, int[] inorder, int preL, int preR, int inL, int inR) {
+		if (preL > preR) {
+			return null;
+		}
+		TreeNode root = new TreeNode(preorder[preL]);
+		int position = 0;
+		for (; position <= inR; position++) {
+			if (inorder[position] == preorder[preL]) {
+				break;
+			}
+		}
+		int leftNum = position - inL;
+		
+		root.left = dfs(preorder, inorder, preL + 1, preL + leftNum, inL, position - 1);
+		root.right = dfs(preorder, inorder, preL + leftNum + 1, preR, position + 1, inR);
+		return root;
+	}
+	
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] preorder = {1, 2, 3};
+		int[] inorder = {2, 1, 3};
+		print(buildTree(preorder, inorder));
+ 	}
+	
+	public static void print(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		
+		System.out.print(root.val + " ");
+		print(root.left);
+		print(root.right);
+	}
+}
