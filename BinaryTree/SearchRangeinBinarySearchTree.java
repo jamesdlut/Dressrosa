7c6581f7cbdcad25314996690b8c449890c95ebc
Peng <eamonpeng91@gmail.com>
2015-05-10 20:04:16 -0400

    Create SearchRangeinBinarySearchTree.java

---
+++ BinaryTree/SearchRangeinBinarySearchTree.java
@@ -0,0 +1,15 @@
+/*
+Search Range in Binary Search Tree
+Given two values k1 and k2 (where k1 < k2) and a root pointer to a Binary Search Tree. Find all the keys of tree in range k1 to k2. i.e. print all x such that k1<=x<=k2 and x is a key of given BST. Return all the keys in ascending order.
+Example
+For example, if k1 = 10 and k2 = 22, then your function should print 12, 20 and 22.
+          20
+
+       /        \
+
+    8           22
+
+  /     \
+
+4       12
+*/
