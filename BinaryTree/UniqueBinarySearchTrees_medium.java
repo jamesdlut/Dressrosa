d232fc3e6622b3176989120a3957c85dd6714a1d
Peng <eamonpeng91@gmail.com>
2015-05-13 06:36:19 +0800

    Update UniqueBinarySearchTrees_medium.java

--- BinaryTree/UniqueBinarySearchTrees_medium.java
+++ BinaryTree/UniqueBinarySearchTrees_medium.java
@@ -11,10 +11,21 @@ Given n = 3, there are a total of 5 unique BST's.
 */
 /*
 The case for 3 elements example
-left nodes possiblities * right nodes possiblities
+left nodes possiblities * right nodes possiblities, 0 - n total n + 1,
 Count[3] = Count[0] * Count[2] (1 as root) 3, 2 > 1, so there is no possible for 3, 2 in left, so 0
               + Count[1] * Count[1] (2 as root) 
               + Count[2] * Count[0] (3 as root) 2, 1 < 3, so there is no possible for 2, 1 in right, so 0
-Therefore, we can get the equation:
-Count[i] = ∑ Count[0, ..., k] * [k + 1, ..., i] 0 <= k < i - 1  
 */
+public class Solution {
+    public int numTrees(int n) {
+        int[] count = new int[n + 1]; 
+        count[0] = 1;
+        count[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                count[i] += count[j] * count[i - j - 1];
+            }
+        }
+        return count[n];
+    }
+}
