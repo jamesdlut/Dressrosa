0d386e3f6c8551ff125434c60fdc8b216ce6a876
Peng <eamonpeng91@gmail.com>
2015-05-12 00:39:18 -0400

    Update UniqueBinarySearchTrees_medium.java

--- BinaryTree/UniqueBinarySearchTrees_medium.java
+++ BinaryTree/UniqueBinarySearchTrees_medium.java
@@ -10,3 +10,12 @@ Given n = 3, there are a total of 5 unique BST's.
     /     /       \                 \
    2     1         2                 3
 */
+/*
+/*
+The case for 3 elements example
+Count[3] = Count[0] * Count[2] (1 as root)
+              + Count[1] * Count[1] (2 as root)
+              + Count[2] * Count[0] (3 as root)
+Therefore, we can get the equation:
+Count[i] = ∑ Count[0...k] * [k+1...i] 0 <= k < i - 1  
+*/
