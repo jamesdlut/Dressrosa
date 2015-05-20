daacc99d744c3b6b35d6e19f7e793ca4e2e8914e
Peng <eamonpeng91@gmail.com>
2015-05-21 06:27:24 +0800

    Update SortList_medium.java

--- LinkedList/SortList_medium.java
+++ LinkedList/SortList_medium.java
@@ -3,5 +3,12 @@ Sort List
 Sort a linked list in O(n log n) time using constant space complexity.
 */
 /*
+our question is to sort whole list, different with binary search, which is to find a target in O(log n) time, 
+T(n)
+T(n / 2) T(n / 2)
+T(n / 2^2) T(n / 2^2) T(n / 2^2) T(n / 2^2)
+...
+T(n / 2^logn) T(n / 2^logn) ... T(n / 2^logn)
+thus, T(n) = T(n / 2) + O(1) = n * T(n / 2^logn) + O(1), that is, O(n log n)
 为什么不用QuickSort? 因为对于链表随机访问太耗时，而heap sort不可行
 */
