24edade412a0d672fa711472f4a499bb0c441599
Peng <eamonpeng91@gmail.com>
2015-05-21 11:02:45 +0800

    Update SortList_medium.java

--- LinkedList/SortList_medium.java
+++ LinkedList/SortList_medium.java
@@ -45,3 +45,165 @@ T(n) <= nT(1) + logn * n = O(n log n)
 (n - 1) + (n - 2) + ... + 1 = n * (n - 1) / 2
 关键字的比较和交换是跳跃进行的, 快速排序是一种不稳定的排序方法
 */
+public class sortListSol {
+	// merge sort
+	public static ListNode sortList1(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		
+		ListNode mid = findMiddle(head);
+		ListNode right = sortList1(mid.next);
+		mid.next = null;
+		ListNode left = sortList1(head);
+		return merge(left, right);
+	}
+	
+	private static ListNode findMiddle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		return slow;
+	}
+	
+	private static ListNode findMiddle2(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+	
+	private static ListNode merge(ListNode head1, ListNode head2) {
+		ListNode dummy = new ListNode(0);
+		ListNode tail = dummy;
+		while (head1 != null && head2 != null) {
+			if (head1.val < head2.val) {
+				tail.next = head1;
+				head1 = head1.next;
+			} else {
+				tail.next = head2;
+				head2 = head2.next;
+			}
+			tail = tail.next;
+		}
+		if (head1 != null) {
+			tail.next = head1;
+		} else {
+			tail.next = head2;
+		}
+		return dummy.next;
+	}
+	// quick sort
+	public static ListNode sortList2(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+		
+		return quickSort(head);
+	}
+	
+	public static boolean isDuplicate(ListNode head) {
+		while (head != null) {
+			if (head.next != null && head.next.val != head.val) {
+				return false;
+			}
+			head = head.next;
+		}
+		return true;
+	}
+	
+	public static ListNode quickSort(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+		
+		if (isDuplicate(head)) {
+			return head;
+		}
+		
+		ListNode headNew = partition(head, head.val);
+		ListNode cur = headNew;
+		ListNode dummy = new ListNode(0);
+		dummy.next = headNew;
+		ListNode pre = dummy;
+		while (cur != null) {
+			if (cur.val == head.val) {
+				break;
+			}
+			cur = cur.next;
+			pre = pre.next;
+		}
+		pre.next = null;
+		ListNode left = dummy.next;
+		ListNode right = cur.next;
+		cur.next = null;
+		left = quickSort(left);
+		right = quickSort(right);
+		if (left != null) {
+			dummy.next = left;
+			while (left.next != null) {
+				left = left.next;
+			}
+			left.next = cur;
+		} else {
+			dummy.next = cur;
+		}
+		cur.next = right;
+		return dummy.next;
+	}
+	
+	public static ListNode partition(ListNode head, int x) {
+		if (head == null) {
+			return null;
+		}
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode pre = dummy;
+		ListNode cur = head;
+		ListNode bigDummy = new ListNode(0);
+		ListNode bigTail = bigDummy;
+		while (cur != null) {
+			if (cur.val >= x) {
+				pre.next = cur.next;
+				bigTail.next = cur;
+				cur.next = null;
+				bigTail = cur;
+			} else {
+				pre = pre.next;
+			}
+			cur = pre.next;
+		}
+		pre.next = bigDummy.next;
+		return dummy.next;
+	}
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ListNode n1 = new ListNode(2);
+		ListNode n2 = new ListNode(8);
+		ListNode n3 = new ListNode(1);
+		ListNode n4 = new ListNode(5);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = null;
+		ListNode rst = sortList1(n1);
+		while (rst != null) {
+			System.out.print(rst.val);
+			rst = rst.next;
+		}
+	}
+}
