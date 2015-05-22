87a6dfe18ed9e1c33d37c1da84067d1928ee30a6
Peng <eamonpeng91@gmail.com>
2015-05-22 10:00:18 +0800

    Update ReorderList_medium.java

--- LinkedList/ReorderList_medium.java
+++ LinkedList/ReorderList_medium.java
@@ -10,6 +10,6 @@ Given {1,2,3,4}, reorder it to {1,4,2,3}.
 4 STEP:
 1. find the mid.
 2. cut the list to two list.
-3. REVERSE the right side.
+3. REVERSE the right side. 注意模板
 4. MERGE the two list.
 */
