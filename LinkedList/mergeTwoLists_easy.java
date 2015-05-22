425f6e3a53a7721000e2928fde061688226136b4
Peng <eamonpeng91@gmail.com>
2015-05-22 16:26:35 +0800

    Update mergeTwoLists_easy.java

--- LinkedList/mergeTwoLists_easy.java
+++ LinkedList/mergeTwoLists_easy.java
@@ -2,3 +2,53 @@
 Merge Two Sorted Lists 
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 */
+public class mergeTwoListsSol {
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(0);
+		ListNode cur = dummy;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				cur.next = l1;
+				l1 = l1.next;
+			} else {
+				cur.next = l2;
+				l2 = l2.next;
+			}
+			cur = cur.next;
+		}
+		if (l1 != null) {
+			cur.next = l1;
+		} else {
+			cur.next = l2;
+		}
+		return dummy.next;
+	}
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(2);
+		ListNode n5 = new ListNode(4);
+		ListNode n6 = new ListNode(6);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = null;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = null;
+		ListNode rst = mergeTwoLists(n1, n4);
+		while (rst != null) {
+			System.out.print(rst.val);
+			rst = rst.next;
+		}
+	}
+}
