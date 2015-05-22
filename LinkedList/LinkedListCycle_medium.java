f168f6a191c34794be2707f7b18d839a9c54f987
Peng <eamonpeng91@gmail.com>
2015-05-22 11:21:52 +0800

    Update LinkedListCycle_medium.java

--- LinkedList/LinkedListCycle_medium.java
+++ LinkedList/LinkedListCycle_medium.java
@@ -4,3 +4,60 @@ Given a linked list, determine if it has a cycle in it.
 Follow up:
 Can you solve it without using extra space?
 */
+public class hasCycleSol {
+	public static boolean hasCycle1(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+		
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean hasCycle2(ListNode head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+		
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n2;
+		System.out.print(hasCycle1(n1));
+		System.out.print(hasCycle2(n1));
+	}
+}
