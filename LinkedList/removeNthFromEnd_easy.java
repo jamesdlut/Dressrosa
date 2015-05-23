ce053818356833b5b5caf1eedbc20e76c72e243e
Peng <eamonpeng91@gmail.com>
2015-05-23 18:23:52 +0800

    Update removeNthFromEnd_easy.java

--- LinkedList/removeNthFromEnd_easy.java
+++ LinkedList/removeNthFromEnd_easy.java
@@ -9,5 +9,31 @@ Given n will always be valid.
 Try to do this in one pass.
 */
 /*
-使用快慢指针, 快指针先行移动N步. 用慢指针指向要移除的Node的前一个Node.
+使用快慢指针, 快指针先行移动n步. 用慢指针指向要移除的Node的前一个Node.
 */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
