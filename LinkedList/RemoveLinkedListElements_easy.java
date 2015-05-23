23bc744e6cd1c7758a75f63acfd7eac33d1e0d4e
Peng <eamonpeng91@gmail.com>
2015-05-23 16:11:57 +0800

    Update RemoveLinkedListElements_easy.java

--- LinkedList/RemoveLinkedListElements_easy.java
+++ LinkedList/RemoveLinkedListElements_easy.java
@@ -5,3 +5,28 @@ Example
 Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5
 */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (head != null) {
+            if (head.val == val) {
+                cur.next = head.next;
+                head = head.next;
+            } else {
+                cur = head;
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
