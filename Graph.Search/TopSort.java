8efa97e6dac3c351cbff9140ffbb8a3fa19a907e
Peng <eamonpeng91@gmail.com>
2015-05-25 10:01:18 +0800

    Create TopSort.java

---
+++ Graph.Search/TopSort.java
@@ -0,0 +1,12 @@
+/*
+Topological Sorting
+Given an directed graph, a topological order of the graph nodes is defined as follow:
+For each directed edge A -> B in graph, A must before B in the order list.
+The first node in the order can be any node in the graph with no nodes direct to it.
+Find any topological order for the given graph.
+Example
+For graph as follow:
+The topological order can be:
+[0, 1, 2, 3, 4, 5]
+[0, 2, 3, 1, 5, 4]
+*/
