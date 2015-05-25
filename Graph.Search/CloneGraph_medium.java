d792fd134ff4d39c70fd6f84e1670abd7d8e257c
Peng <eamonpeng91@gmail.com>
2015-05-25 09:50:06 +0800

    Update CloneGraph_medium.java

--- Graph.Search/CloneGraph_medium.java
+++ Graph.Search/CloneGraph_medium.java
@@ -26,3 +26,94 @@ SOL 2
 题目虽然简单，但考虑递归的特性使程序简洁. 比如: 我们拷贝只拷贝根节点, 而子节点的拷贝由recursion来完成, 这样可以使程序更加简洁.
 注意: 要先加入到map, 再调用rec, 否则会造成不断地反复拷贝而死循环.
 */
+public class CloneGraphSol {
+	// SOL 1 BFS
+	public static UndirectedGraphNode cloneGraph1(UndirectedGraphNode node) {
+		if (node == null) {
+			return null;
+		}
+		
+		HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+		Queue<UndirectedGraphNode> q = new LinkedList<UndirectedGraphNode>();
+		q.offer(node);
+		UndirectedGraphNode nodeCopy = new UndirectedGraphNode(node.label);
+		map.put(node, nodeCopy);
+		while (!q.isEmpty()) {
+			UndirectedGraphNode cur = q.poll();
+			UndirectedGraphNode curCopy = map.get(cur);
+			for (UndirectedGraphNode child : cur.neighbors) {
+				if (map.containsKey(child)) {
+					curCopy.neighbors.add(map.get(child));
+				} else {
+					q.offer(child);
+					UndirectedGraphNode childCopy = new UndirectedGraphNode(child.label);
+					curCopy.neighbors.add(childCopy);
+					map.put(child, childCopy);
+				}
+			}
+		}
+		return map.get(node);
+	}
+	// SOL 2 DFS
+	public static UndirectedGraphNode cloneGraph2(UndirectedGraphNode node) {
+		if (node == null) {
+			return null;
+		}
+		return rec(node, new HashMap<UndirectedGraphNode, UndirectedGraphNode>());
+	}
+	
+	public static UndirectedGraphNode rec(UndirectedGraphNode node, HashMap<UndirectedGraphNode, UndirectedGraphNode> map) {
+		if (map.containsKey(node)) {
+			return map.get(node);
+		}
+		UndirectedGraphNode nodeCopy = new UndirectedGraphNode(node.label);
+		map.put(node, nodeCopy);
+		for (int i = 0; i < node.neighbors.size(); i++) {
+			nodeCopy.neighbors.add(rec(node.neighbors.get(i), map));
+		}
+		return nodeCopy;
+	}
+	
+	public static class UndirectedGraphNode {
+		int label;
+		List<UndirectedGraphNode> neighbors;
+		UndirectedGraphNode(int x) {
+			label = x;
+			neighbors = new ArrayList<UndirectedGraphNode>();
+		}
+	}
+	
+	public static void main(String[] args) {
+		UndirectedGraphNode n1 = new UndirectedGraphNode(0);
+		UndirectedGraphNode n2 = new UndirectedGraphNode(1);
+		UndirectedGraphNode n3 = new UndirectedGraphNode(2);
+		n1.neighbors.add(n2);
+		n1.neighbors.add(n3);
+		n2.neighbors.add(n3);
+		n2.neighbors.add(n1);
+		n3.neighbors.add(n3);
+		n3.neighbors.add(n1);
+		n3.neighbors.add(n2);
+		UndirectedGraphNode rst = cloneGraph2(n1);
+		HashMap<UndirectedGraphNode, Integer> hash = new HashMap<UndirectedGraphNode, Integer>();
+		Queue<UndirectedGraphNode> q = new LinkedList<UndirectedGraphNode>();
+		q.offer(rst);
+		hash.put(rst, rst.label);
+		while (!q.isEmpty()) {
+			UndirectedGraphNode cur = q.poll();
+			for (UndirectedGraphNode child : cur.neighbors) {
+				if (!hash.containsKey(child)) {
+					q.offer(child);
+					hash.put(child, child.label);
+				} else {
+					continue;
+				}
+			}
+		}
+		System.out.print(hash.values().toString());
+	}
+}
+/*
+ * output:
+ * [0, 1, 2]
+ */
