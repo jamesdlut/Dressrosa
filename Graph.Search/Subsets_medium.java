c33178c8d45aa96b81f5fa52ea3f3bf8b7d3115e
Peng <eamonpeng91@gmail.com>
2015-05-29 23:34:24 +0800

    Update Subsets_medium.java

--- Graph.Search/Subsets_medium.java
+++ Graph.Search/Subsets_medium.java
@@ -17,3 +17,18 @@ If nums = [1,2,3], a solution is:
   []
 ]
 */
+/*
+Basically our algorithm can't be faster than O(2^n) since we need to go through all possible combinations.
+SOL 1 Recurstion
+SOL 2 The idea is that all the numbers from 0 to 2^n are represented by unique bit strings of n bit width that can be translated into a subset.
+e.g.
+Nr Bits Combination
+0  000  {}
+1  001  {1}
+2  010  {2}
+3  011  {1, 2}
+4  100  {3}
+5  101  {1, 3}
+6  110  {2, 3}
+7  111  {1, 2, 3]
+*/
