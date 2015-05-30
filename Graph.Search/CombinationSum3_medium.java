6cf4161d21af812e247f3d61753f3c5af9e7d4ba
Peng <eamonpeng91@gmail.com>
2015-05-30 23:07:58 +0800

    Update CombinationSum3_medium.java

--- Graph.Search/CombinationSum3_medium.java
+++ Graph.Search/CombinationSum3_medium.java
@@ -11,3 +11,26 @@ Input: k = 3, n = 9
 Output:
 [[1,2,6], [1,3,5], [2,3,4]]
 */
+public class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        List<Integer> path = new ArrayList<Integer>();
+        dfs(rst, 1, 0, k, n, path);
+        return rst;
+    }
+    
+    private void dfs(List<List<Integer>> rst, int start, int sum, int k, int n, List<Integer> path) {
+        if (sum > n || path.size() > k) {
+            return;
+        }
+        if (path.size() == k && sum == n) {
+            rst.add(new ArrayList<Integer>(path));
+        } else {
+            for (int i = start; i <= 9; i++) {
+                path.add(i);
+                dfs(rst, i + 1, sum + i, k, n, path);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+}
