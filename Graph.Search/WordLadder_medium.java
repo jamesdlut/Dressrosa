187a83cdcbdefe5213e90813d7441d17f739995f
Peng <eamonpeng91@gmail.com>
2015-05-30 23:27:21 +0800

    Update WordLadder_medium.java

--- Graph.Search/WordLadder_medium.java
+++ Graph.Search/WordLadder_medium.java
@@ -15,3 +15,13 @@ Return 0 if there is no such transformation sequence.
 All words have the same length.
 All words contain only lowercase alphabetic characters.
 */
+/*
+经典的BFS题目
+想象一下, 这个变换过程是一个树, 每一层是当前所有的变换结果, 下一层又是上一层的字符串的所有的变换结果.
+e.g.
+HIT
+AIT, BIT, CIT, DIT...
+HAT, HBT, HCT, HDT...
+HIA, HIB, HIC, HID...
+HIT可以有这么多种变换方式, 而AIT, BIT本身也可以以相同的方式展开, 这就形成了一个相当大的树
+*/
