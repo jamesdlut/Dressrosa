659da5db90e3ad5a525470020646930c480059d4
Peng <eamonpeng91@gmail.com>
2015-05-30 10:59:39 +0800

    Create Subsets2_medium.java

---
+++ Graph.Search/Subsets2_medium.java
@@ -0,0 +1,18 @@
+/*
+Subsets II
+Given a list of numbers that may has duplicate numbers, return all possible subsets
+Example
+If S = [1,2,2], a solution is:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+Note
+Each element in a subset must be in non-descending order.
+The ordering between two subsets is free.
+The solution set must not contain duplicate subsets.
+*/
