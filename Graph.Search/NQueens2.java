44a242ce8e133b31441262b1ae21e37b94cab21c
Peng <eamonpeng91@gmail.com>
2015-05-30 13:59:41 +0800

    Create NQueens2.java

---
+++ Graph.Search/NQueens2.java
@@ -0,0 +1,7 @@
+/*
+N-Queens II
+Follow up for N-Queens problem.
+Now, instead outputting board configurations, return the total number of distinct solutions.
+Example
+For n=4, there are 2 distinct solutions.
+*/
