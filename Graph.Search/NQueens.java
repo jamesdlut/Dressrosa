0407b64c54a73faa1d4be7cadf14982b18cba224
Peng <eamonpeng91@gmail.com>
2015-05-30 13:41:25 +0800

    Update NQueens.java

--- Graph.Search/NQueens.java
+++ Graph.Search/NQueens.java
@@ -18,3 +18,75 @@ There exist two distinct solutions to the 4-queens puzzle:
 Challenge
 Can you do it without recursion?
 */
+/*
+SOL 1
+cols: 存放每一行皇后的列值, 我们在每一行放一个皇后
+DFS 某一行中所有的位置, 看是否可以放置一个皇后
+若加新皇后在row = cols.size() - 1行的下一行的第col列, 则新皇后的行号和列号是:
+(cols.size(), col)
+但若棋盘上已有的某皇后位置, 比如(i, cols.get(i)), 与新皇后位置互为对角线, 则不可行
+*/
+public class NQueensSol {
+	public static List<String[]> solveNQueens1(int n) {
+		List<String[]> rst = new ArrayList<String[]>();
+		List<Integer> cols = new ArrayList<Integer>();
+		if (n <= 0) {
+			return rst;
+		}
+		helper(n, cols, rst);
+		return rst;
+	}
+	
+	public static String[] createSol(int n, List<Integer> cols) {
+		String[] chessboard = new String[n];
+		for (int i = 0; i < n; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < n; j++) {
+				if (j == cols.get(i)) {
+					sb.append('Q');
+				} else {
+					sb.append('.');
+				}
+			}
+			chessboard[i] = sb.toString();
+		}
+		return chessboard;
+	}
+	
+	private static boolean isValid(List<Integer> cols, int col) {
+		for (int i = 0; i < cols.size(); i++) {
+			if (col == cols.get(i)) {
+				return false;
+			}
+			if (cols.size() - i == Math.abs(col - cols.get(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static void helper(int n, List<Integer> cols, List<String[]> rst) {
+		if (cols.size() == n) {
+			rst.add(createSol(n, new ArrayList<Integer>(cols)));
+			return;
+		}
+		for (int col = 0; col < n; col++) {
+			if (!isValid(cols, col)) {
+				continue;
+			}
+			cols.add(col);
+			helper(n, cols, rst);
+			cols.remove(cols.size() - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		List<String[]> list = solveNQueens1(4);
+		for (String[] str : list) {
+			for (String s : str) {
+				System.out.println(s);
+			}
+			System.out.print("\n");
+		}
+	}
+}
