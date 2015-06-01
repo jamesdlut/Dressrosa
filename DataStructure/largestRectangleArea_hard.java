52343aca4ada8aaee51a87f0503a557dcf4b6d15
Peng <eamonpeng91@gmail.com>
2015-06-01 13:24:38 +0800

    Update largestRectangleArea_hard.java

--- DataStructure/largestRectangleArea_hard.java
+++ DataStructure/largestRectangleArea_hard.java
@@ -7,3 +7,34 @@ For example,
 Given height = [2,1,5,6,2,3],
 return 10.
 */
+/*
+O(n) runtime
+*/
+public class Solution {
+    public int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        
+        Stack<Integer> s = new Stack<Integer>();
+        int len = height.length;
+        int max = 0;
+        int i = 0;
+        while (i <= len) {
+            if (s.isEmpty() || (i < len && height[i] >= height[s.peek()])) {
+                s.push(i);
+                i++;
+            } else {
+                int h = height[s.pop()];
+                int width;
+                if (s.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - s.peek() - 1;
+                }
+                max = Math.max(max, h * width);
+            }
+        }
+        return max;
+    }
+}
