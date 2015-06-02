d326df52ed0481b34771d33c0a6947ffb2f9cf63
Peng <eamonpeng91@gmail.com>
2015-06-02 21:05:40 +0800

    Update WordDictionary_medium.java

--- DataStructure/WordDictionary_medium.java
+++ DataStructure/WordDictionary_medium.java
@@ -19,3 +19,85 @@ You should be familiar with how a Trie works. If not, please work on this proble
 /*
 字典树 + dfs
 */
+public class WordDictSol {
+	public static class WordDictionary {
+		private TrieNode root;
+		
+		public WordDictionary() {
+			root = new TrieNode();
+		}
+		// Adds a word into a data structure.
+		public void addWord(String word) {
+			TrieNode cur = root;
+			int len = word.length();
+			for (int i = 0; i < len; i++) {
+				int c = word.charAt(i) - 'a';
+				if (cur.children[c] == null) {
+					cur.children[c] = new TrieNode();
+				}
+				cur = cur.children[c];
+			}
+			cur.have = true;
+		}
+		// Returns if the word is in the data structure. A word could
+		// contain the dot character '.' to represent any one letter.
+		public boolean search(String word) {
+			TrieNode cur = root;
+			int len = word.length();
+			return dfs(cur, word, 0, len);
+		}
+		
+		private boolean dfs(TrieNode node, String word, int pos, int len) {
+			if (node == null || (pos == len && !node.have)) {
+				return false;
+			}
+			if (pos == len && node.have) {
+				return true;
+			}
+			if (word.charAt(pos) == '.') {
+				for (int i = 0; i < 26; i++) {
+					boolean tmp = dfs(node.children[i], word, pos + 1, len);
+					if (tmp) {
+						return tmp;
+					}
+				}
+				return false;
+			} else {
+				int c = word.charAt(pos) - 'a';
+				return dfs(node.children[c], word, pos + 1, len);
+			}
+		}
+		/*
+		public boolean search(String word) {
+			TrieNode cur = root;
+			int len = word.length();
+			for (int i = 0; i < len; i++) {
+				int c = word.charAt(i) - 'a';
+				if (cur.children[c] == null) {
+					return false;
+				}
+				cur = cur.children[c];
+			}
+			return cur.have;
+		}
+		*/
+	}
+	
+	public static class TrieNode {
+		boolean have;
+		TrieNode[] children;
+		public TrieNode() {
+			have = false;
+			children = new TrieNode[26];
+		}
+	}
+	// Your WordDictionary object will be instantiated and called as such:
+	// WordDictionary wordDictionary = new WordDictionary();
+	// wordDictionary.addWord("word");
+	// wordDictionary.search("pattern");
+	public static void main(String[] args) {
+		WordDictionary wordDictionary = new WordDictionary();
+		wordDictionary.addWord("word");
+		System.out.print(wordDictionary.search("pattern"));
+	}
+}
