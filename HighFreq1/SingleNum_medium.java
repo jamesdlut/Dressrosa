ddb3868acb77bc69081bdee10dc84113807cd5fb
Peng <eamonpeng91@gmail.com>
2015-06-03 23:11:55 +0800

    Update SingleNum_medium.java

--- HighFreq1/SingleNum_medium.java
+++ HighFreq1/SingleNum_medium.java
@@ -5,3 +5,31 @@ Given an array of integers, every element appears twice except for one. Find tha
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int cur = 0;
+        for (int i = 0; i < nums.length; i++) {
+            cur ^= nums[i];
+        }
+        return cur;
+        /*
+        for (int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            int cnt = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (cur == nums[j]) {
+                    cnt++;
+                } 
+            }
+            if (cnt == 1) {
+                return cur;
+            }
+        }
+        return -1;
+        */
+    }
+}
