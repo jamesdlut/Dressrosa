962c8e2f99e77dee7f2a92014fbc3e545d1b2fa1
Peng <eamonpeng91@gmail.com>
2015-06-03 13:07:31 +0800

    Update findKthLargest_medium.java

--- DataStructure/findKthLargest_medium.java
+++ DataStructure/findKthLargest_medium.java
@@ -14,3 +14,72 @@ note: quickselect worst case O(n^2) best/average case O(n)
 Choose the last one as the pivot
 left: the first one which is bigger than pivot. Change pivot.
 */
+public class KthLargestSol {
+	// SOL 1 Quick Sort
+	public static int findKthLargest1(int[] nums, int k) {
+		if (k < 1 || nums == null) {
+			return 0;
+		}
+		
+		return helper1(nums.length - k + 1, nums, 0, nums.length - 1);
+	}
+	
+	public static int helper1(int x, int[] nums, int start, int end) {
+		int pivot = nums[end];
+		int left = start;
+		int right = end;
+		while (true) {
+			while (nums[left] < pivot && left < right) {
+				left++;
+			}
+			while (nums[right] >= pivot && right > left) {
+				right--;
+			}
+			if (left == right) {
+				break;
+			}
+			swap(nums, left, right);
+		}
+		swap(nums, left, end);
+		if (x == left + 1) {
+			return pivot;
+		} else if (x < left + 1) {
+			return helper1(x, nums, start, left - 1);
+		} else {
+			return helper1(x, nums, left + 1, end);
+		}
+	}
+	
+	public static void swap(int[] nums, int n1, int n2) {
+		int tmp = nums[n1];
+		nums[n1] = nums[n2];
+		nums[n2] = tmp;
+	}
+	// SOL 2 min heap
+	public static int findKthLargest2(int[] nums, int k) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		
+		int len = nums.length;
+		Queue<Integer> heap = new PriorityQueue<Integer>();
+		for (int i = 0; i < k; i++) {
+			heap.offer(nums[i]);
+		}
+		for (int j = k; j < len; j++) {
+			if (nums[j] > heap.peek()) {
+				heap.poll();
+				heap.offer(nums[j]);
+			} else {
+				continue;
+			}
+		}
+		return heap.peek();
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {3,2,1,5,6,4};
+		int k = 2;
+		System.out.print(findKthLargest2(nums, k));
+	}
+}
