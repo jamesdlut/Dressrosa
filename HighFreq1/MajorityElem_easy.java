337f7a18701cfac2710b21ead756195c722077f4
Peng <eamonpeng91@gmail.com>
2015-06-05 00:37:55 +0800

    Update MajorityElem_easy.java

--- HighFreq1/MajorityElem_easy.java
+++ HighFreq1/MajorityElem_easy.java
@@ -7,25 +7,44 @@ You may assume that the array is non-empty and the majority element always exist
 两个数不一样就抵消掉
 要对majority number进行检查, 以排除不存在majority number的情况. 如1, 2, 3, 4这样的数列, 是没有majory number的.
 */
-public class Solution {
-    public int majorityElement(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        
-        int maj = nums[0];
-        int len = nums.length;
-        int cnt = 0;
-        for (int i = 0; i < len; i++) {
-            if (cnt == 0) {
-                maj = nums[i];
-                cnt++;
-            } else if (nums[i] == maj) {
-                cnt++;
-            } else {
-                cnt--;
-            }
-        }
-        return maj;
-    }
+public class MajorNumSol {
+	public static int majorityElement(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+		
+		int maj = nums[0];
+		int len = nums.length;
+		// SOL 1 Voting
+		int cnt = 0;
+		for (int i = 0; i < len; i++) {
+			if (cnt == 0) {
+				maj = nums[i];
+				cnt++;
+			} else if (nums[i] == maj) {
+				cnt++;
+			} else {
+				cnt--;
+			}
+		}
+		// SOL 2 Examing
+		/*
+		cnt = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == maj) {
+				cnt++;
+			}
+		}
+		*/
+		if (cnt <= nums.length / 2) {
+			return -1;
+		}
+		
+		return maj;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 4};
+		System.out.print(majorityElement(nums));
+	}
 }
