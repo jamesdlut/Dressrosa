c634c08f8660d0f8cb55e78161885fae72667c4b
Peng <eamonpeng91@gmail.com>
2015-06-04 23:42:18 +0800

    Update MajorityElem_easy.java

--- HighFreq1/MajorityElem_easy.java
+++ HighFreq1/MajorityElem_easy.java
@@ -3,3 +3,23 @@ Majority Element
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 You may assume that the array is non-empty and the majority element always exist in the array.
 */
+/*
+两个数不一样就抵消掉
+*/
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int val = 0;
+        int cnt = 0;
+        for (int i : nums) {
+            if (cnt == 0) {
+                val = i;
+                cnt++;
+            } else if (val == i) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        return val;
+    }
+}
