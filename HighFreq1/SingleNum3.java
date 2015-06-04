c0e09549495ef6cb01d3c72e2534f85de0806911
Peng <eamonpeng91@gmail.com>
2015-06-04 22:00:24 +0800

    Create SingleNum3.java

---
+++ HighFreq1/SingleNum3.java
@@ -0,0 +1,8 @@
+/*
+Single Number III
+Given 2 * n + 2 numbers, every numbers occurs twice except two, find them.
+Example
+Given [1, 2, 2, 3, 4, 4, 5, 3] return 1 and 5
+Challenge
+O(n) time, O(1) extra space.
+*/
