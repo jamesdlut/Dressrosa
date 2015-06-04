98c8de91c4d01de2d6ba93ad8b43477338948f55
Peng <eamonpeng91@gmail.com>
2015-06-05 00:18:12 +0800

    Update MajorityElem_easy.java

--- HighFreq1/MajorityElem_easy.java
+++ HighFreq1/MajorityElem_easy.java
@@ -5,21 +5,27 @@ You may assume that the array is non-empty and the majority element always exist
 */
 /*
 两个数不一样就抵消掉
+要对majority number进行检查, 以排除不存在majority number的情况. 如1, 2, 3, 4这样的数列, 是没有majory number的.
 */
 public class Solution {
     public int majorityElement(int[] nums) {
-        int val = 0;
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int maj = nums[0];
+        int len = nums.length;
         int cnt = 0;
-        for (int i : nums) {
+        for (int i = 0; i < len; i++) {
             if (cnt == 0) {
-                val = i;
+                maj = nums[i];
                 cnt++;
-            } else if (val == i) {
+            } else if (nums[i] == maj) {
                 cnt++;
             } else {
                 cnt--;
             }
         }
-        return val;
+        return maj;
     }
 }
