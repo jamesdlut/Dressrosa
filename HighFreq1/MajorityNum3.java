9a731d9c8f7ba614c1284fa366951be526ac3ee8
Peng <eamonpeng91@gmail.com>
2015-06-05 12:49:38 +0800

    Create MajorityNum3.java

---
+++ HighFreq1/MajorityNum3.java
@@ -0,0 +1,10 @@
+/*
+Majority Number III
+Given an array of integers and a number k, the majority number is the number that occurs more than 1/k of the size of the array. Find it.
+Note
+There is only one majority number in the array.
+Example
+For [3,1,2,3,2,3,3,4,4,4] and k = 3, return 3
+Challenge
+O(n) time and O(k) extra space
+*/
