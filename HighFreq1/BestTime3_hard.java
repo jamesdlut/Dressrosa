051a240a33b24438dc6c8ceb9d74d6ecece45d33
Peng <eamonpeng91@gmail.com>
2015-06-05 21:55:46 +0800

    Update BestTime3_hard.java

--- HighFreq1/BestTime3_hard.java
+++ HighFreq1/BestTime3_hard.java
@@ -21,3 +21,66 @@ O(n^2)的算法很容易想到:
 已知left[i], 求left[i + 1]. 已知right[i], 求right[i - 1].
 用O(n)的时间找出最大的left[i] + right[i]
 */
+public class BestTime3Sol {
+	// SOL 1
+	public static int maxProfit1(int[] prices) {
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
+		
+		int len = prices.length;
+		int[] left = new int[len];
+		int[] right = new int[len];
+		
+		int min = prices[0];
+		left[0] = 0;
+		for (int i = 1; i < len; i++) {
+			min = Math.min(min, prices[i]);
+			left[i] = Math.max(left[i - 1], prices[i] - min);
+		}
+		
+		int max = prices[len - 1];
+		right[len - 1] = 0;
+		for (int i = len - 2; i >= 0; i--) {
+			max = Math.max(max, prices[i]);
+			right[i] = Math.max(right[i + 1], max - prices[i]);
+		}
+		
+		int rst = 0;
+		for (int i = 0; i < len; i++) {
+			rst = Math.max(rst, left[i] + right[i]);
+		}
+		return rst;
+    }
+	// SOL 2 DP
+	public static int maxProfit2(int[] prices) {
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
+		
+		int rst = 0;
+		int len = prices.length;
+		int[] left = new int[len];
+		
+		int min = prices[0];
+		left[0] = 0;
+		for (int i = 1; i < len; i++) {
+			min = Math.min(min, prices[i]);
+			left[i] = Math.max(left[i - 1], prices[i] - min);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		int profit = 0;
+		for (int i = len - 1; i >= 0; i--) {
+			max = Math.max(max, prices[i]);
+			profit = Math.max(profit, max - prices[i]);
+			rst = Math.max(rst, profit + left[i]);
+		}
+		return rst;
+	}
+	
+	public static void main(String[] args) {
+		int[] prices = {2, 3, 10, 6, 4, 8, 1};
+		System.out.print(maxProfit2(prices));
+	}
+}
