7bb8f68a2898a29576b65612ed78d5f73f4d94b7
Peng <eamonpeng91@gmail.com>
2015-06-07 23:35:20 +0800

    Create MinSubArray.java

---
+++ HighFreq1/MinSubArray.java
@@ -0,0 +1,31 @@
+/*
+Minimum Subarray
+Given an array of integers, find the subarray with smallest sum.
+Return the sum of the subarray.
+Note
+The subarray should contain at least one integer.
+Example
+For [1, -1, -2, 1], return -3
+*/
+public class MinSubSol {
+	public static int minSubArray(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		
+		int min = Integer.MAX_VALUE;
+		int sum = 0;
+		int maxSum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			min = Math.min(min, sum - maxSum);
+			maxSum = Math.max(sum, maxSum);
+		}
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1, -1, -2, 1};
+		System.out.print(minSubArray(nums));
+	}
+}
