9498da7da54785b816ce8eb4f2403a40ef47fade
Peng <eamonpeng91@gmail.com>
2015-06-07 18:16:10 +0800

    Update MaxSubarray_medium.java

--- HighFreq1/MaxSubarray_medium.java
+++ HighFreq1/MaxSubarray_medium.java
@@ -6,3 +6,25 @@ the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 More practice:
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
+/*
+滑动窗口
+*/
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            
+            sum += nums[i];
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
