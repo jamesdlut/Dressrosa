63a98282be9221e8319bf4ba2ea36f7e18a231f2
Peng <eamonpeng91@gmail.com>
2015-06-07 18:47:16 +0800

    Create MaxSubarray3.java

---
+++ HighFreq1/MaxSubarray3.java
@@ -0,0 +1,10 @@
+/*
+Maximum Subarray III
+Given an array of integers and a number k, find k non-overlapping subarrays which have the largest sum.
+The number in each subarray should be contiguous.
+Return the largest sum.
+Note
+The subarray should contain at least one number
+Example
+Given [-1,4,-2,3,-2,3],k=2, return 8
+*/
