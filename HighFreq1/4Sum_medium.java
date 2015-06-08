27dd3af9924ad37f3ac1806e449aabd33e2a96e5
Peng <eamonpeng91@gmail.com>
2015-06-08 16:23:56 +0800

    Update 4Sum_medium.java

--- HighFreq1/4Sum_medium.java
+++ HighFreq1/4Sum_medium.java
@@ -10,3 +10,50 @@ A solution set is:
 (-2, -1, 1, 2)
 (-2,  0, 0, 2)
 */
+/*
+time O(nlogn) + O(n^2) * O(n) = O(n^3)
+*/
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        rst.add(list);
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+        return rst;
+    }
+}
