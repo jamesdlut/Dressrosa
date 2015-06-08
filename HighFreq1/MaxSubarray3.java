95a17b838b148ce25d24d51f90534547ba887503
Peng <eamonpeng91@gmail.com>
2015-06-08 22:26:51 +0800

    Update MaxSubarray3.java

--- HighFreq1/MaxSubarray3.java
+++ HighFreq1/MaxSubarray3.java
@@ -9,39 +9,72 @@ Example
 Given [-1,4,-2,3,-2,3],k=2, return 8
 */
 /*
-DP d[i][j] means the maximum sum we can get by selecting i subarrays from the first j elements.
+SOL 1
+d[i][j] means the maximum sum we can get by selecting j subarrays from the first i elements.
+SOL 2
+DP d[i][j] means the maximum sum we can get by selecting j subarrays from the first i elements.
 time O(len^2 * k)
 */
 public class MaxSubarray3Sol {
-	public static int maxSubArray(int[] nums, int k) {
+	public static int maxSubArray1(int[] nums, int k) {
+		int len = nums.length;
+		if (len < k) {
+			return 0;
+		}
+		
+		int[][] d = new int[len + 1][k + 1];
+		for (int i = 1; i <= len; i++) {
+			for (int j = 1; j <= k; j++) {
+				if (i < j) {
+					d[i][j] = 0;
+					continue;
+				}
+				d[i][j] = Integer.MIN_VALUE;
+				for (int p = j - 1; p <= i - 1; p++) {
+					int local = nums[p];
+					int global = local;
+					for (int t = p + 1; t <= i - 1; t++) {
+						local = Math.max(local + nums[t], nums[t]);
+						global = Math.max(local, global);
+					}
+					if (d[i][j] < d[i][j - 1] + global) {
+						d[i][j] = d[p][j - 1] + global;
+					}
+				}
+			}
+		}
+		return d[len][k];
+	}
+	
+	public static int maxSubArray2(int[] nums, int k) {
 		if (nums.length < k) {
 			return 0;
 		}
 		
 		int len = nums.length;
-		int[][] d = new int[k + 1][len + 1];
+		int[][] d = new int[len + 1][k + 1];
 		for (int i = 0; i <= len; i++) {
-			d[0][i] = 0;
+			d[i][0] = 0;
 		}
 		
-		for (int i = 1; i <= k; i++) {
-			for (int j = i; j <= len; j++) {
+		for (int j = 1; j <= k; j++) {
+			for (int i = j; i <= len; i++) {
 				d[i][j] = Integer.MIN_VALUE;
 				int endMax = 0;
 				int max = Integer.MIN_VALUE;
-				for (int p = j - 1; p >= i - 1; p--) {
+				for (int p = i - 1; p >= j - 1; p--) {
 					endMax = Math.max(nums[p], endMax + nums[p]);
 					max = Math.max(endMax, max);
-					d[i][j] = Math.max(d[i][j], d[i - 1][p] + max);
+					d[i][j] = Math.max(d[i][j], d[p][j - 1] + max);
 				}
 			}
 		}
-		return d[k][len];
+		return d[len][k];
 	}
 	
 	public static void main(String[] args) {
 		int[] nums = {-1, 4, -2, 3, -2, 3};
 		int k = 2;
-		System.out.print(maxSubArray(nums, k));
+		System.out.print(maxSubArray2(nums, k));
 	}
 }
