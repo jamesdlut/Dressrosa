01608ed7ec5c84d6c93cadaed29a34410876afff
Peng <eamonpeng91@gmail.com>
2015-06-08 13:35:48 +0800

    Update MinSubLen_medium.java

--- HighFreq1/MinSubLen_medium.java
+++ HighFreq1/MinSubLen_medium.java
@@ -7,10 +7,13 @@ More practice:
 If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
 */
 /*
+SOL 1 Sliding Window
 滑动窗口, 若小于s, 把右边界往右滑动扩大窗口, 否则把左边界往右滑, 缩小窗口, 所有的数都是正数
+SOL 2 Binary Search 
+sums[i] - target = sums[i] - sums[i] + s - 1 < sums[i] - sums[end] = s
 */
 public class Solution {
-    public int minSubArrayLen(int s, int[] nums) {
+    public int minSubArrayLen1(int s, int[] nums) {
         if (nums.length < 1) {
             return 0;
         }
@@ -36,5 +39,52 @@ public class Solution {
         } else {
             return rst;
         }
+    }
+    
+    public int minSubArrayLen2(int s, int[] nums) {
+        int len = nums.length;
+        int[] sums = new int[len + 1];
+        sums[0] = 0;
+        for (int i = 1; i < len + 1; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        
+        int rst = Integer.MAX_VALUE;
+        for (int i = 1; i < len + 1; i++) {
+            if (sums[i] >= s) {
+                int end = binarySearch(0, i, sums[i] - s + 1, sums);
+                if (end != -1 && i - end < rst) {
+                    rst = i - end;
+                }
+            }
+        }
+        
+        if (rst == Integer.MAX_VALUE) {
+            return 0;
+        } else {
+            return rst;
+        }
+    }
+    
+    private int binarySearch(int start, int end, int target, int[] sums) {
+        if (start >= end) {
+            return -1;
+        }
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (sums[mid] == target) {
+                end = mid - 1;
+            } else if (sums[mid] > target) {
+                end = mid - 1;
+            } else if (sums[mid] < target) {
+                start = mid;
+            }
+        }
+        if (sums[end] < target) {
+            return end;
+        } else if (sums[start] < target) {
+            return start;
+        } else return -1;
     }
 }
