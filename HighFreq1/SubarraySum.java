c73a20481c8be6d9d78fc46d22ce31b843149eb8
Peng <eamonpeng91@gmail.com>
2015-06-08 13:43:52 +0800

    Create SubarraySum.java

---
+++ HighFreq1/SubarraySum.java
@@ -0,0 +1,6 @@
+/*
+Subarray Sum
+Given an integer array, find a subarray where the sum of numbers is zero. Your code should return the index of the first number and the index of the last number.
+Example
+Given [-3, 1, 2, -3, 4], return [0, 2] or [1, 3].
+*/
