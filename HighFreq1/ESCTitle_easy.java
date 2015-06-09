a1e5eafde54386d5154c2fbcbbd737ce83d4a2c3
Peng <eamonpeng91@gmail.com>
2015-06-09 21:19:41 +0800

    Create ESCTitle_easy.java

---
+++ HighFreq1/ESCTitle_easy.java
@@ -0,0 +1,32 @@
+/*
+Excel Sheet Column Title
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+*/
+/*
+Excel序是这样的: A~Z, AA~ZZ, AAA~ZZZ, ...
+本质上就是将一个10进制数转换为一个26进制数
+*/
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        if (n < 1) {
+            return "";
+        } else {
+            while (n > 0) {
+                n--;
+                char c = (char) (n % 26 + 'A');
+                sb.append(c);
+                n /= 26;
+            }
+            return sb.reverse().toString();
+        }
+    }
+}
