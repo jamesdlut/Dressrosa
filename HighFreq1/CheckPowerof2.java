fd43193cf41303364d4be3e19629ae51de5542fa
Peng <eamonpeng91@gmail.com>
2015-06-09 20:39:10 +0800

    Update CheckPowerof2.java

--- HighFreq1/CheckPowerof2.java
+++ HighFreq1/CheckPowerof2.java
@@ -7,3 +7,35 @@ For n=5, return false;
 Challenge
 O(1) time
 */
+/*
+bit manipulation
+1的个数只能有1个才是power of 2
+*/
+public class CheckPowSol {
+	public static boolean checkPowerOf21(int n) {
+		if (n <= 0) {
+			return false;
+		}
+
+		if ((n & (n - 1)) == 0) {
+			return true;
+		} else return false;
+	}
+	
+	public static boolean checkPowerOf22(int n) {
+		int cnt = 0;
+		for (int i = 0; i < 31; i++) {
+			if (((n >> i) & 1) == 1) {
+				cnt++;
+			} 
+		}
+		if (cnt == 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(checkPowerOf21(Integer.MIN_VALUE));
+	}
+}
