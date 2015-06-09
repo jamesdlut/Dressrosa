1354fbd3f57ec3f99e6b44abaf16d8fb127bdd78
Peng <eamonpeng91@gmail.com>
2015-06-09 19:34:23 +0800

    Update InterleavingNum.java

--- HighFreq1/InterleavingNum.java
+++ HighFreq1/InterleavingNum.java
@@ -13,3 +13,73 @@ Do it in-place and without extra memory.
 2. Have more Positive numbers. Reverse the array. 
 3. Reorder the negative and the positive numbers.
 */
+public class InterleaveNumSol {
+	public static int[] rerange(int[] nums) {
+		if (nums == null || nums.length <= 2) {
+			return nums;
+		}
+		
+		int len = nums.length;
+		int cntPositive = 0;
+		
+		for (int num : nums) {
+			if (num > 0) {
+				cntPositive++;
+			}
+		}
+		
+		int i1 = 0;
+		for (int i2 = 0; i2 < len; i2++) {
+			if (nums[i2] > 0) {
+				swap(nums, i1, i2);
+				i1++;
+			}
+		}
+		
+		int posPointer = 1;
+		int negPointer = 0;
+		if (cntPositive > nums.length / 2) {
+			posPointer = 0;
+			negPointer = 1;
+			int left = 0;
+			int right = len - 1;
+			while (left < right) {
+				swap(nums, left, right);
+				left++;
+				right--;
+			}
+		}
+		
+		while (true) {
+			while (posPointer < len && nums[posPointer] > 0) {
+				posPointer += 2;
+			}
+			while (negPointer < len && nums[negPointer] < 0) {
+				negPointer += 2;
+			}
+			if (posPointer >= len || negPointer >= len) {
+				break;
+			}
+			swap(nums, posPointer, negPointer);
+		}
+		return nums;
+	}
+	
+	private static void swap(int[] nums, int left, int right) {
+		int tmp = nums[left];
+		nums[left] = nums[right];
+		nums[right] = tmp;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {-1, -2, -3, 4, 5, 6};
+		rerange(nums);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+	}
+}
+/*
+ * outputs:
+ * -1 5 -3 4 -2 6 
+ */
