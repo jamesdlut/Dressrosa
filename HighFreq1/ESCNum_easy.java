6f8951d063f99b18da6fc25b614671c9c83f21e2
Peng <eamonpeng91@gmail.com>
2015-06-09 21:31:35 +0800

    Update ESCNum_easy.java

--- HighFreq1/ESCNum_easy.java
+++ HighFreq1/ESCNum_easy.java
@@ -12,4 +12,20 @@ For example:
 */
 /*
 26进制转10进制, 注意以'A'而不是0开头, 因此要“+1”
+e.g. 
+Input: "BA"
+Output:	27
+i 0 
+num = 0 * 26 + 'B' - 'A' + 1 = 2
+i 1
+num = 1 * 26 + 'A' - 'A' + 1 = 27
 */
+public class Solution {
+    public int titleToNumber(String s) {
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            num = num * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        return num;
+    }
+}
