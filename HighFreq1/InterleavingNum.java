65fabd96041bde760bad0131833990e9337b21c7
Peng <eamonpeng91@gmail.com>
2015-06-09 17:43:32 +0800

    Create InterleavingNum.java

---
+++ HighFreq1/InterleavingNum.java
@@ -0,0 +1,10 @@
+/*
+Interleaving Positive and Negative Numbers
+Given an array with positive and negative integers. Re-range it to interleaving with positive and negative integers.
+Note
+You are not necessary to keep the original order or positive integers or negative integers.
+Example
+Given [-1, -2, -3, 4, 5, 6], after re-range, it will be [-1, 5, -2, 4, -3, 6] or any other legal answer.
+Challenge
+Do it in-place and without extra memory.
+*/
