7361eba4962a6e26c20516db5a33f4fdc2515737
Peng <eamonpeng91@gmail.com>
2015-06-09 17:32:32 +0800

    Update SortLetter.java

--- HighFreq1/SortLetter.java
+++ HighFreq1/SortLetter.java
@@ -6,3 +6,29 @@ It's not necessary to keep the original order of lower-case letters and upper ca
 Example
 For "abAcD", a reasonable answer is "acbAD"
 */
+public class SortLettersSol {
+	public static void sortLetters(char[] nums) {
+		int low = 0;
+		int cap = nums.length - 1;
+		while (low < cap) {
+			while (low < cap && nums[low] >= 'a' && nums[low] <= 'z') {
+				low++;
+			}
+			while (low < cap && nums[cap] >= 'A' && nums[cap] <= 'Z') {
+				cap--;
+			}
+			
+			char tmp = nums[low];
+			nums[low] = nums[cap];
+			nums[cap] = tmp;
+		}
+	}
+	
+	public static void main(String[] args) {
+		char[] nums = {'a', 'b', 'A', 'c', 'D'};
+		sortLetters(nums);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+	}
+}
