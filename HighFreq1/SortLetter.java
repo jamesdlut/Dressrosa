84da69846aa7e82640538212027f69f803d2ebe8
Peng <eamonpeng91@gmail.com>
2015-06-09 17:26:42 +0800

    Create SortLetter.java

---
+++ HighFreq1/SortLetter.java
@@ -0,0 +1,8 @@
+/*
+Sort Letters by Case
+Given a string which contains only letters. Sort it by lower case first and upper case second.
+Note
+It's not necessary to keep the original order of lower-case letters and upper case letters.
+Example
+For "abAcD", a reasonable answer is "acbAD"
+*/
