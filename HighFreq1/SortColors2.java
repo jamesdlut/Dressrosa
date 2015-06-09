12e09ed294162014f4779007be3ba2bc23a7df0d
Peng <eamonpeng91@gmail.com>
2015-06-09 15:54:48 +0800

    Create SortColors2.java

---
+++ HighFreq1/SortColors2.java
@@ -0,0 +1,11 @@
+/*
+Sort Colors II
+Given an array of n objects with k different colors (numbered from 1 to k), sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
+注意
+You are not suppose to use the library's sort function for this problem.
+样例
+Given colors=[3, 2, 2, 1, 4], k=4, your code should sort colors in-place to [1, 2, 2, 3, 4]. 
+挑战
+A rather straight forward solution is a two-pass algorithm using counting sort. That will cost O(k) extra memory.
+Can you do it without using extra memory?
+*/
