cd4177514467095438173dd84ccb5c543b7badd5
Peng <Eamon4213@users.noreply.github.com>
2015-07-27 20:47:44 -0400

    Update Rotate_Image_medium.java

--- HighFreq2/Rotate_Image_medium.java
+++ HighFreq2/Rotate_Image_medium.java
@@ -28,4 +28,30 @@ public class Solution {
             }
         }
     }
+    // SOL 2 in-place
+    public void rotate2(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        } 
+        
+        int n = matrix.length;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        
+        while (n > 1) {
+            for (int i = 0; i < n - 1; i++) {
+                int tmp = matrix[top][left + i];
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = tmp;
+            }
+            top++;
+            right--;
+            left++;
+            bottom--;
+            
+            n = n - 2;
+        }
+        return;
+    }
 }
