639b482aac9c2127959aeb3d22fc0e716f749f68
Peng <Eamon4213@users.noreply.github.com>
2015-07-27 20:27:10 -0400

    Update Rotate_Image_medium.java

--- HighFreq2/Rotate_Image_medium.java
+++ HighFreq2/Rotate_Image_medium.java
@@ -5,3 +5,27 @@ Rotate the image by 90 degrees (clockwise).
 Follow up:
 Could you do this in-place?
 */
+public class Solution {
+    // SOL 1 naive solution
+    public void rotate1(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        
+        int m = matrix.length;
+        
+        int[][] rst = new int[m][m];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                rst[j][m - 1 - i] = matrix[i][j];
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = rst[i][j];
+            }
+        }
+    }
+}
