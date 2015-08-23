c0a6ab6d579a14abcf6cf199adcf131ab816245d
Peng <Eamon4213@users.noreply.github.com>
2015-08-23 16:44:31 -0400

    Update 70_ClimbStairs_easy.java

--- part_2/DP.plus/70_ClimbStairs_easy.java
+++ part_2/DP.plus/70_ClimbStairs_easy.java
@@ -25,7 +25,6 @@ public class Solution {
 // 斐波那契数列：0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 // 注：此时a1=0，a2=1，an=a(n-1)+a(n-2)（n>=2,n∈N*）
 // 即：裴波那契数列的第n项的值是第n阶楼梯的爬法的种类数
-// 另外关于tail recurisive，思路和dp差不多，但是会需要多余的space
 // time O(N) space O(N)
 public class Solution {
     public int climbStairs(int n) {
@@ -41,3 +40,4 @@ public class Solution {
         return sum;
     }
 }
+// SOL 3 tail recursive
