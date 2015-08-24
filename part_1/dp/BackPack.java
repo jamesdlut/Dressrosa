d6208a43be5a8719d6e7e3e5020bca513050bc3a
Peng <Eamon4213@users.noreply.github.com>
2015-08-24 18:29:09 -0400

    Update BackPack.java

--- part_1/dp/BackPack.java
+++ part_1/dp/BackPack.java
@@ -20,7 +20,10 @@ time O(N*target)
 /*
 f[i][j] “前i”个数,取出一些能否凑成j, here j is the target number
 */
-// This is a typical backpack question.
+/*
+quote from http://love-oriented.com/pack/#sec1
+*/
+// This is a typical 0-1 backpack question.
 // state: we can create a two-dimensional boolean array f[i][S] to record all possible cases, the first sequence represents the first i items, the second sequence represents the target number.
 // function: we use f[i][S] = f[i - 1][S - A[i]] this function to go through all possible cases in a loop.
 // initialize: we can first initialize all of the f[i][S] boolean values to false in a loop.
